6358b8356eb9172ccc89c05d1b59bae147116fc5
PozhidaevNikolay <pozhidaev63nik@gmail.com>
2025-07-21 10:33:07 +0400

    Initial

---
+++ src/test/java/org/example/ratingapp/RestaurantRatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ratingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
